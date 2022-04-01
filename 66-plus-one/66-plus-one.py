e12987ffc6e02dafc688d377a153f23304510e3e
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-01 14:25:57 -0400

    Time: 38 ms (71.96%), Space: 13.9 MB (13.49%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        s = ""
+        
+        for i in digits:
+            s = s + str(i)
+        s = str(int(s) + 1)
+        
+        return list(s)
+        
