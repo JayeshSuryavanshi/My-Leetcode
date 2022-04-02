0501f0176ba00974fd6ab54caa1d9ff961822857
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-02 19:13:33 -0400

    Delete 520. Detect capital.py

--- 520. Detect capital.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def detectCapitalUse(self, word):
-        """
-        :type word: str
-        :rtype: bool
-        """
-        
-        return word.isupper() or word.islower() or word.istitle()
