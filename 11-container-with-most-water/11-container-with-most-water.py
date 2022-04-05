ddc9c9144fe6b54182bfe6b526f528cbaa1a4b49
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-05 02:09:40 -0400

    Time: 895 ms (59.18%), Space: 27.5 MB (17.98%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -4,21 +4,14 @@ class Solution:
         line_height = len(height)
         left, right = 0, line_height - 1
         max_width = line_height - 1
-        
         area = 0
-        
         for width in range(max_width, 0, -1):
-            
-            # incrementing from left while calculating the area
             if height[left] < height[right]:
                 area = max(area, width * height[left])
                 left = left + 1
-            
-            # decrementing from right while calculating the area
             else:
                 area = max(area, width * height[right])
                 right = right - 1 
-                
         return area
         
         
