74f5c664d4963f7a967eb9d30d61377e6e173dca
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-06 12:53:56 -0400

    Time: 57 ms (24.82%), Space: 13.8 MB (57.21%) - LeetHub

--- 231-power-of-two/231-power-of-two.py
+++ 231-power-of-two/231-power-of-two.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        number = n
-        complement = (n & n - 1)
-        return number and not complement
+        return n and not (n & n - 1)
             
         
