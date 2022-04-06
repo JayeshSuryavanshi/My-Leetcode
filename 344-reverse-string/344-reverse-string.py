9dfc7a242eef9608c9139f021dae2127ac32cdac
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-06 02:38:37 -0400

    Time: 343 ms (19.19%), Space: 18.5 MB (19.06%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,7 +1,3 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        
         s[:] = s[::-1]
