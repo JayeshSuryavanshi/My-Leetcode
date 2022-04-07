d24e7cde484b47913c9ecdb017d97ece34e2b029
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-07 02:57:36 -0400

    Time: 39 ms (91.93%), Space: 13.9 MB (71.59%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,9 +1,7 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        # converted number to binary 
         num1 = bin(int(num1))
         num2 = bin(int(num2))
-        # binaries cannot be added like normal addition; format different
         sum = bin(int(num1, 2) + int(num2, 2))
         return str(int(sum, 2))
         
