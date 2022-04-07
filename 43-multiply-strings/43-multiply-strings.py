6907678ab821045838e3ae4e6f600fe5e2189d50
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-07 03:07:34 -0400

    Time: 46 ms (78.44%), Space: 13.8 MB (77.81%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,6 +1,4 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        num1 = bin(int(num1))
-        num2 = bin(int(num2))
-        mult = bin(int(num1, 2) * int(num2, 2))
+        mult = bin(int(bin(int(num1)), 2) * int(bin(int(num2)), 2))
         return str(int(mult, 2))
