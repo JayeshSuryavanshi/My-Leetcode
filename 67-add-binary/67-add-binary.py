b5c27b891204f3e9be07594bf41fae1c5a7a36eb
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-10 01:17:41 -0400

    Time: 28 ms (98.15%), Space: 13.9 MB (27.83%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,8 +1,10 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        # string -> int -> decimal
         A = int(a, 2)
         B = int(b, 2)
         sum = A + B
+        # sum in binary format, then split at b, -1 is to return last element
         sum_binary = bin(sum).split('b')[-1]
         return sum_binary
         
