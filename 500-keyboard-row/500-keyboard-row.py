a30543ecb89e9224076b1257cfcd2218e7aec36d
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-10 20:14:29 -0400

    Time: 42 ms (53.27%), Space: 13.9 MB (71.49%) - LeetHub

---
+++ 500-keyboard-row/500-keyboard-row.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        first = 'qwertyuiop'
+        second = 'asdfghjkl'
+        third = 'zxcvbnm'
+        final = []
+        
+        for word in words:
+            if word[0].lower() in first:
+                if all(chars in first for chars in word.lower()):
+                    final.append(word)
+            if word[0].lower() in second:
+                if all(chars in second for chars in word.lower()):
+                    final.append(word)
+            if word[0].lower() in third:
+                if all(chars in third for chars in word.lower()):
+                    final.append(word)          
+        return final
+        
+        
+                
