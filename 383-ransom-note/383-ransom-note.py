6dab1a54f1ef828356d98bc5a629e33c9bcd006c
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-19 12:37:31 -0400

    Time: 95 ms (35.52%), Space: 14.2 MB (54.88%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,5 +1,6 @@
 from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        a, b= Counter(ransomNote), Counter(magazine)
+        a = Counter(ransomNote)
+        b = Counter(magazine)
         return not collections.Counter(ransomNote) - collections.Counter(magazine)
