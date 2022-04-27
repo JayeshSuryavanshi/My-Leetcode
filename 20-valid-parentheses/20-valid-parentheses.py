afa517ee7db5513efbd4439ee009d54b07ac1739
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-26 20:00:51 -0400

    Time: 62 ms (12.85%), Space: 13.9 MB (26.14%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,8 +5,7 @@ class Solution:
         for i in s:
             if i in mapping:
                 stack.append(i)
-            elif len(stack) == 0 \
-            or mapping[stack.pop()] != i:
+            elif len(stack) == 0 or mapping[stack.pop()] != i:
                 return False
         return len(stack) == 0 
         
