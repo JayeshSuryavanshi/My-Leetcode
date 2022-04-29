c8eae9849848cf0ea983d3c753b1a361c362f7a9
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-29 14:44:03 -0400

    Time: 50 ms (33.64%), Space: 13.8 MB (72.80%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -9,34 +9,3 @@ class Solution:
             elif len(stack) == 0 or pairs[stack.pop()] != i:
                 return False
         return len(stack) == 0
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # _ = {'(':')', '{':'}','[':']'}
-        # stack = []
-        # for i in s:
-        #     if i in _:
-        #         stack.append(i)
-        #     elif len(stack) == 0 or _[stack.pop()] != i:
-        #         return False
-        # return len(stack) == 0 
-        
