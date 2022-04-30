2a12abe340ebc1c0936f1020477110863126ffd9
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-04-30 18:32:37 -0400

    Time: 33 ms (75.10%), Space: 13.9 MB (63.06%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
@@ -5,7 +5,8 @@ class Solution:
         
         map = '0123456789abcdef'
         final = ''
-        if num<0: num += 2 ** 32
+        if num<0: 
+            num = num + 2 ** 32
         while num > 0:
             digit = num % 16
             num = (num-digit) // 16
