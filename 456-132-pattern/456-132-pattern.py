0a5b8f8e315a61d6c9e64e93beb739ac4e06620a
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-05-06 21:03:56 -0400

    Time: 370 ms (85.81%), Space: 32.2 MB (12.38%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,13 +1,5 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        
-#         nums_cpy = sorted(nums)
-#         print(nums)
-#         print(nums_cpy)
-#         if nums_cpy == nums:
-#             return False
-#         return True
-
         stack = []
         neg = float(-inf)
         rev = nums[::-1]
