eec34cbe9cd4fc7991c69f1b3cc3650b4d4a48cb
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-05-09 17:36:13 -0400

    Time: 55 ms (18.67%), Space: 13.9 MB (79.63%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        
+        mapping = {    '2':'abc',
+                       '3':'def', 
+                       '4':'ghi',
+                       '5':'jkl',
+                       '6':'mno',
+                       '7':'pqrs',
+                       '8':'tuv',
+                       '9':'wxyz'
+                  }
+
+        def recursive(i=0, combo="", out=[]):
+            if i == len(digits):
+                out.append(combo)
+            else:
+                nextDigit = digits[i]
+                children = mapping[nextDigit]
+                for child in children:
+                    recursive(i+1, combo+child, out)
+            
+            return out        
+        return recursive(0, "", [])
