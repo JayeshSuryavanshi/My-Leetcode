b531272cf61a570574754fcacd57582169a2ac28
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-05-31 16:16:42 -0400

    Time: 381 ms (83.84%), Space: 27.2 MB (26.20%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,7 +1,6 @@
-import itertools
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        sample = list(itertools.product([0,1], repeat = k))            
+        # sample = list(itertools.product([0,1], repeat = k))            
         return len({s[i:i + k] for i in range(len(s) - k + 1)}) == 2 ** k
 
         
