19f080b1f864c78583efdd736ba603218326f615
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-06-08 19:50:54 -0400

    Time: 40 ms (51.08%), Space: 13.9 MB (53.78%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -1,3 +1,13 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
-        return int(s==s[::-1]) or 2
+#         return int(s==s[::-1]) or 2
+
+        # Two pointer approach
+        def zip_iter():
+            i,j,n = 0, len(s)-1, len(s) // 2
+            while i<=n:
+                yield (s[i], s[j])
+                i += 1
+                j -= 1
+                
+        return 1 if all(x==y for x,y in zip_iter()) else 2
