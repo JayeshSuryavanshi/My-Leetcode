e46a27c4ece0a8537f6685841a7eb13a757cc902
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-06-15 12:47:20 -0400

    Time: 105 ms (51.00%), Space: 15.1 MB (49.26%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,16 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashset = {}
-        
-#         for num in nums:
-#             potentialMatch = target - num
-#             if potentialMatch in hashset:
-#                 return [nums.index(potentialMatch), nums.index(num)]
-#             else:
-#                 hashset[num] = True
-                
-#         return []
-        
+
         dictionary = {}
         
         for i in range(len(nums)):
