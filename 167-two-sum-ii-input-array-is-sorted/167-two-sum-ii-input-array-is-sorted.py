c144a3ad78d00c6c472fa319d0bda99092c96a9a
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-06-15 13:58:31 -0400

    Time: 297 ms (7.53%), Space: 15 MB (10.95%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,15 +1,24 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         print(numbers)
-        i = 0
-        j = len(numbers)-1
+        dictionary = {}
         
-        while i < j:
-            potentialMatch = numbers[i] + numbers[j]
-            if potentialMatch == target:
-                return [i + 1, j + 1]
-            elif potentialMatch < target:
-                i = i + 1
+        for i in range(len(numbers)):
+            if target-numbers[i] in dictionary:
+                return [dictionary[target-numbers[i]] + 1, i + 1] # dictionary lookup
             else:
-                j = j - 1
+                dictionary[numbers[i]] = i #filling dictionary
+                
+                
+#         i = 0
+#         j = len(numbers)-1
+        
+#         while i < j:
+#             potentialMatch = numbers[i] + numbers[j]
+#             if potentialMatch == target:
+#                 return [i + 1, j + 1]
+#             elif potentialMatch < target:
+#                 i = i + 1
+#             else:
+#                 j = j - 1
                 
