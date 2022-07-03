4998bc77e64f6ff99b28e4e571017ae647def06c
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-07-03 16:37:09 -0400

    Time: 43 ms (61.59%), Space: 13.9 MB (9.84%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,11 +1,23 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        # Naive Approach - Square Root 
+        # Naive Approach - Square Root - TC: O(sqrt(n))
         
-        for i in range(1, num + 1):
-            if i * i == num:
+        # for i in range(1, num + 1):
+        #     if i * i == num:
+        #         return True
+        #     elif i * i > num:
+        #         return False
+        
+        # Binary Search Approach - TC: O(log(n))
+        
+        left, right = 1, num
+
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid > num:
+                right = mid - 1         
+            elif mid * mid < num:
+                left = mid + 1
+            else:
                 return True
-            elif i * i > num:
-                return False
-        
-        
+        return False
