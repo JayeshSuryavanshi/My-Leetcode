16340c3232267a96fa496912879658a585bc5562
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-07-13 00:53:41 -0400

    Time: 80 ms (45.84%), Space: 14.5 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,6 @@
+from collections import Counter 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        p = ''.join(sorted(s))
-        q = ''.join(sorted(t))
-        return p == q
+        c1 = Counter(s)
+        c2 = Counter(t)
+        return c1 == c2
