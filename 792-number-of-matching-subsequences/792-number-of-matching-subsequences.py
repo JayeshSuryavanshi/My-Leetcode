06a7ca124aaf3c5ec2d92dc97805d7e79602aef1
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-07-20 19:14:14 -0400

    Time: 472 ms (86.51%), Space: 15.5 MB (75.71%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -6,9 +6,9 @@ class Solution:
             flag = 1
             for ch in word:
                 idx = s.find(ch,i)
-                if idx==-1:
+                if idx == -1:
                     flag = 0
                     break
                 i = idx+1
-            if flag : res+=1
+            if flag : res += 1
         return res
