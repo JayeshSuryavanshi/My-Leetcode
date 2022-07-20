5e787d17496d3d6e8d5aa0a3e5c560b9d7fed8e5
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-07-20 19:13:44 -0400

    Time: 308 ms (98.86%), Space: 15.5 MB (75.71%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        res = 0
+        for word in words:
+            i =0
+            flag = 1
+            for ch in word:
+                idx = s.find(ch,i)
+                if idx==-1:
+                    flag = 0
+                    break
+                i = idx+1
+            if flag : res+=1
+        return res
