ded6fd9f6083a1ca2fcafb0cf89357f357106a61
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-08-03 19:01:27 -0400

    Time: 492 ms (45.77%), Space: 15.1 MB (16.56%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -0,0 +1,12 @@
+from sortedcontainers import SortedList
+
+class MyCalendar:
+    def __init__(self):
+        self.books = SortedList([(float('-inf'), float('-inf')), (float('inf'), float('inf'))])
+
+    def book(self, start: int, end: int) -> bool:
+        idx = self.books.bisect_left((start, end))
+        if start < self.books[idx-1][1] or end > self.books[idx][0]:
+            return False
+        self.books.add((start, end))
+        return True
