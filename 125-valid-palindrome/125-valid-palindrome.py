f7bff0ea9e83aa7426535cec37a36d636e6c2a17
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-08-13 13:09:29 -0400

    Time: 81 ms (43.31%), Space: 14.7 MB (42.08%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,11 +1,6 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:  
-        
-        # re.sub(pattern, repl, string)
-        # This regex can practically clean any impure string
-        new_string = re.sub("[^a-zA-Z0-9]+","",s).lower()
-        
-        # checking if reversed string is equal to the original string
-        return new_string[:] == new_string[::-1]
-        
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(e for e in s if e.isalnum()).lower()
+        return s==s[::-1]
+                
         
