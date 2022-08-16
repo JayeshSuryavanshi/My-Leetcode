4c89bd7ef729add1a6c81803aa8bd97d9d91f73f
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-08-16 13:20:42 -0400

    Time: 60 ms (99.50%), Space: 14.2 MB (59.01%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,12 +1,9 @@
-from collections import Counter
-
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        # counter object initialized as count
-        count = Counter(s)
-        
-        # counter stores element:count(key)
-        for indx in count.keys():
-            if count[indx] == 1:
-                return s.index(indx)
+        freq = collections.Counter(s)
+        for idx in freq.keys():
+            if freq[idx] == 1:
+                return s.index(idx)
         return -1
+        
+        
