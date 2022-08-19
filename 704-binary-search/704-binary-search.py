97b8bfa2d658917e9d641b0327c4d7f7d3d06480
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-08-19 00:44:34 -0400

    Time: 326 ms (66.23%), Space: 15.4 MB (73.46%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,6 +1,5 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-            lo, hi = 0, None
-            if not hi: hi = len(nums)
+            lo, hi = 0, len(nums)
             pos = bisect_left(nums, target, lo, hi)
             return pos if pos != hi and nums[pos] == target else -1
