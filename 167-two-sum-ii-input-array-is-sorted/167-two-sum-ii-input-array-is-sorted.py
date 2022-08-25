fa9e23cd5a77775ef281136719935d075209205a
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-08-25 01:29:56 -0400

    Time: 180 ms (65.72%), Space: 15 MB (42.04%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -9,16 +9,3 @@ class Solution:
             else:
                 dictionary[numbers[i]] = i #filling dictionary
                 
-                
-#         i = 0
-#         j = len(numbers)-1
-        
-#         while i < j:
-#             potentialMatch = numbers[i] + numbers[j]
-#             if potentialMatch == target:
-#                 return [i + 1, j + 1]
-#             elif potentialMatch < target:
-#                 i = i + 1
-#             else:
-#                 j = j - 1
-                
