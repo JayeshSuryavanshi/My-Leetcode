980055042ef84a33c46fe122361c7d6a9cacb190
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-08-25 22:35:34 -0400

    Time: 46 ms (78.07%), Space: 13.9 MB (26.74%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        res = sorted(int(x) for x in str(n))
+        print(res)
+        for i in range(30):
+            if sorted([int(x) for x in str(1 << i)]) == res: 
+                return True
+        return False
