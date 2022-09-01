dbcad09df44f595afa6129df81178b426d0b8d0f
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-08-31 17:22:16 -0700

    Time: 49 ms (98.54%), Space: 14 MB (42.60%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,11 +1,10 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        cities_1 = []
-        cities_2 = []
-        
-        for path in paths:
-            cities_1.append(path[0])
-            cities_2.append(path[1])
-        for path in cities_2:
-            if path not in cities_1: return path
+        pairs = {}
+        for i in paths:
+            pairs[i[0]] = i[1]
+			
+        for src, dest in paths:
+            if dest not in pairs:
+                return dest
         
