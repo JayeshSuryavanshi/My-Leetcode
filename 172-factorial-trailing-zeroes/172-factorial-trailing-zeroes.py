94a32cd75d64a4d2e75bf4c0bb477928dde4c73a
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-09-04 16:32:10 -0700

    Time: 4363 ms (12.38%), Space: 14 MB (12.56%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -1,7 +1,5 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        
-        # using a factorial function
         def factorial(num):
             fact = 1
             for i in range(1, num+1):
@@ -9,11 +7,8 @@ class Solution:
             return fact
         
         num_factorial = str(factorial(n))
-        print(num_factorial)
-        
         num_factorial = num_factorial[::-1]
-        print(num_factorial)
-            
+        
         i = 0
         count = 0
         while i < len(num_factorial):
