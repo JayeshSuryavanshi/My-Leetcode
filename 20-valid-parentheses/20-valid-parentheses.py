15cfe935e8b262d7de947fcb199e2556003b2a5d
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-09-06 13:54:09 -0700

    Time: 44 ms (64.92%), Space: 13.9 MB (72.15%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -2,13 +2,12 @@ class Solution:
     def isValid(self, s: str) -> bool:
         pairs = {'(':')', '{':'}', '[':']'}
         stack = []
-        # "()[]{}"
-        # "(]"
-        for i in s:
-            if i in pairs:
-                stack.append(i)
+        
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
             elif len(stack) == 0:
                 return False
-            elif pairs[stack.pop()] != i:
+            elif pairs[stack.pop()] != bracket:
                 return False
         return len(stack) == 0
