25f3629764f31783cd2325a49ce60c924ff1b54c
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-09-09 13:16:58 -0400

    Time: 4806 ms (9.13%), Space: 67 MB (79.12%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -3,7 +3,6 @@ class Solution:
         properties.sort(key = lambda x: (-x[0],x[1]))
         result = 0
         maximum = 0
-        print(properties)
         for attack, defence in properties:
             if (defence < maximum):
                 result += 1
