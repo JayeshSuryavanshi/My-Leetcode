66a02d3f028a06877497e20631ff84b0151a843c
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-09-12 20:05:36 -0400

    Time: 267 ms (8.50%), Space: 17.1 MB (96.74%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,6 +1,50 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dictionary = defaultdict(list)
-        for word in strs:
-            dictionary[''.join(sorted(word))].append(word)
-        return dictionary.values()
+        dictionary = {}
+        
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+            
+            if sorted_string not in dictionary:
+                dictionary[sorted_string] = []
+            dictionary[sorted_string].append(string)
+            
+        return list(dictionary.values())
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         strs_table = {}
+
+#         for string in strs:
+#             sorted_string = ''.join(sorted(string))
+
+#             if sorted_string not in strs_table:
+#                 strs_table[sorted_string] = []
+
+#             strs_table[sorted_string].append(string)
+
+#         return list(strs_table.values())
