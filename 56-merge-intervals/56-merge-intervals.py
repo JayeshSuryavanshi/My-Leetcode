5f667d2e133aaa3c307e121ab366e48d3cf0b716
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-09-12 21:26:12 -0400

    Time: 335 ms (10.77%), Space: 18 MB (85.13%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -3,11 +3,8 @@ class Solution:
         if len(intervals) == 1:
             return intervals
         intervals.sort()
-        
         i, j = 0, 1
-        
         while j < len(intervals):
-            
             if intervals[i][1] >= intervals[j][0]:
                 intervals[i][1] = max(intervals[i][1], intervals[j][1])
                 intervals.pop(j)   
