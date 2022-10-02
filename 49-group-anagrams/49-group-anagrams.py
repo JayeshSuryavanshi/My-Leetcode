15bcf5d489e0e46b620bc17ea644971e73d1a289
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-10-02 17:03:33 -0400

    Time: 260 ms (16.23%), Space: 17.3 MB (80.93%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,7 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # collections - defaultdict
         dictionary = collections.defaultdict(list)
+        # traverse every string in strings
         for st in strs:
             s = ''.join(sorted(st))
+            # in defaultdict we can append just like list
             dictionary[s].append(st)
+            # returning the dictionary values in 
         return sorted(dictionary.values())
