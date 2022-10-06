a9f6707d522a6bdd022ee06c52e0e7a68c20ba4f
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-10-05 20:00:49 -0400

    Time: 5551 ms (17.51%), Space: 76.7 MB (66.93%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -1,39 +1,20 @@
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        node_idx = 0
-        graph = defaultdict(list)
-        for u, v in connections:
-            graph[u].append(v)
-            graph[v].append(u)
-
-        
-        assignedid = [None] * n
-        rank = [None] * n
-        res = []
-
-        def dfs(node, parent):
-            nonlocal node_idx
-            if rank[node] is not None:
-                return
-
-            assignedid[node] = rank[node] = node_idx  # Preorder Id Assignment "on the fly"
-            node_idx += 1
-
-            # 1. Preorder DFS on Undirected Graph
-            for neigh in graph[node]:
-                if neigh != parent:
-                    dfs(neigh, node)
-                    # Update Rank Postorder
-                    if rank[neigh] < rank[node]:
-                        rank[node] = rank[neigh]
-
-            # 2. Critical connection
-            # Neighbor Rank > Original Node means no cycle in between exists
-            for neigh in graph[node]:
-                if neigh != parent and rank[neigh] > assignedid[node]:
-                    res.append([neigh, node])
-                    
-
-        dfs(0, None)
-
-        return res
+        edgeMap = defaultdict(list)
+        for a,b in connections:
+            edgeMap[a].append(b)
+            edgeMap[b].append(a)
+        disc, low, time, ans = [0] * n, [0] * n, [1], []
+        def dfs(curr: int, prev: int):
+            disc[curr] = low[curr] = time[0]
+            time[0] += 1
+            for next in edgeMap[curr]:
+                if not disc[next]:
+                    dfs(next, curr)
+                    low[curr] = min(low[curr], low[next])
+                elif next != prev:
+                    low[curr] = min(low[curr], disc[next])
+                if low[next] > disc[curr]:
+                    ans.append([curr, next])
+        dfs(0, -1)
+        return ans
